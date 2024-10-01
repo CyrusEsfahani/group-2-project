6ece9f284d9c34801570935bfa30c411d51b1e6c
Heisenberg <esfahani.cyrus@gmail.com>
2024-09-30 20:25:51 -0700

    updated app.tsx, main.tsx along with components

---
+++ server/components/signup.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Signup = () => {
+  const [formData, setFormData] = useState({ username: '', email: '', password: '' });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('/api/auth/signup', formData);
+      console.log(res.data);
+    } catch (error) {
+      console.error(error.response.data);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="username" placeholder="Username" onChange={handleChange} />
+      <input type="email" name="email" placeholder="Email" onChange={handleChange} />
+      <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default Signup;
